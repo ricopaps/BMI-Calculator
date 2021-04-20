77b32d7796d15fe7db5740b88cc903dcb5939bf7
ricopaps <enrico.carlos150237@my.lsgh.edu.ph>
2021-04-20 10:29:33 +0800

    1 file modified, 1 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -4,6 +4,7 @@ import { NavController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+  styleUrls: ['home.css']
 })
 export class HomePage {
 height:number;
